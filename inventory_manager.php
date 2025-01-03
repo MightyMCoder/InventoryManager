8d510bb1f8fd8b63f6b72afb8d6ffa17a28d8324
MightyMCoder <130976036+MightyMCoder@users.noreply.github.com>
2025-01-03 23:34:25 +0100

    Update copyright and license information to the GNU GPL v3.0

--- inventory_manager.php
+++ inventory_manager.php
@@ -3,21 +3,34 @@
  ***********************************************************************************************
  * InventoryManager
  *
- * Version 1.0.3
+ * Version 1.0.6
  *
  * InventoryManager is an Admidio plugin for managing the inventory of an organisation.
  * 
- * Note:
+ * Compatible with Admidio version 4.3
+ * 
+ * @see         https://github.com/MightyMCoder/InventoryManager/ The InventoryManager GitHub project
+ * @author      MightyMCoder
+ * @copyright   2024 - today MightyMCoder
+ * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU General Public License v3.0 only
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License (https://github.com/MightyMCoder/InventoryManager/blob/master/LICENSE)
+ * 
+ * 
+ * @note:
  *  - InventoryManager is based on KeyManager by rmbinder (https://github.com/rmbinder/KeyManager)
  *  - InventoryManager uses the external class XLSXWriter (https://github.com/mk-j/PHP_XLSXWriter)
  * 
- * Author: MightyMCoder
- *
- * Compatible with Admidio version 4.3
- * 
- * @copyright The Admidio Team
- * @see https://www.admidio.org/
- * @license https://www.gnu.org/licenses/gpl-2.0.html GNU General Public License v2.0 only
  * 
  * Parameters:
  * 
